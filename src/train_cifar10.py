c496586d22051a00744ec8d10a9576118521b37a
Real Limoges <rlimoges@Reals-MacBook-Pro-2.local>
2017-01-26 13:20:01 -0800

    refactor loading files

--- src/train_cifar10.py
+++ src/train_cifar10.py
@@ -4,10 +4,7 @@ numpy arrays, and then builds and trains a CNN.
 
 '''
 import cPickle
-import glob
 import numpy as np
-
-
 
 
 def unpack_data(filename):
@@ -20,16 +17,15 @@ def unpack_data(filename):
 
     return data_dict
 
-def build_model():
-    pass    
 
-if __name__ == '__main__':
-    training_data = glob.glob('../data/data_batch_*')
-    
-    X_train = np.empty((0,32*32*3))
+def load_training():
+    '''
+    Loads and reshapes the data for the training set
+    '''
+    X_train = np.empty((0, 32 * 32 * 3))
     y_train = np.empty(1)
 
-    for i in xrange(1,6):
+    for i in xrange(1, 5):
         fname = '../data/data_batch_{}'.format(i)
         data_dict = unpack_data(fname)
 
@@ -39,9 +35,55 @@ if __name__ == '__main__':
         else:
             X_train = np.vstack((X_train, data_dict['data']))
             y_train = np.hstack((y_train, data_dict['labels']))
-   
-    foo = X_train.reshape(X_train.shape[0], 3, 32, 32)
-    from PIL import Image
 
-    img = Image.fromarray(foo[0], 'RGB')
-    img.show()
+    X_train = X_train.reshape(
+        (X_train.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
+
+    return (X_train, y_train)
+
+
+def load_validation():
+    '''
+    Loads and reshapes the data for the validation set
+    '''
+
+    X_valid = np.empty((0, 32 * 32 * 3))
+    y_valid = np.empty(1)
+
+    fname = '../data/data_batch_4'
+    data_dict = unpack_data(fname)
+
+    X_valid = data_dict['data']
+    y_valid = data_dict['labels']
+
+    X_valid = X_valid.reshape(
+        (X_valid.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
+
+    return (X_valid, y_valid)
+
+
+def load_testing():
+    '''
+    Loads and reshapes the data for the test set
+    '''
+
+    X_test = np.empty((0, 32 * 32 * 3))
+    y_test = np.empty(1)
+
+    fname = '../data/test_batch'
+    data_dict = unpack_data(fname)
+
+    X_test = data_dict['data']
+    y_test = data_dict['labels']
+
+    X_test = X_test.reshape(
+        (X_test.shape[0], 3, 32, 32)).transpose(0, 2, 3, 1)
+
+    return (X_test, y_test)
+
+
+if __name__ == '__main__':
+
+    X_train, y_train = load_training()
+    X_valid, y_valid = load_validation()
+    X_test, y_test = load_testing()
